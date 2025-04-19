8fb65eff43ed9c4fa764699d865de90b82a3ad7d
furkansayar <furkan42sayar@gmail.com>
2025-04-19 23:44:29 +0300

    Update MovieSection.tsx

--- frontend/src/pages/home/MovieSection.tsx
+++ frontend/src/pages/home/MovieSection.tsx
@@ -6,31 +6,12 @@ interface MovieSectionProps {
   children: ReactNode;
 }
 
-const MovieSection: React.FC<MovieSectionProps> = ({ title, link, children }) => {
+const MovieSection: React.FC<MovieSectionProps> = ({ title, children }) => {
   return (
     <section>
       <div className="movie-section-header">
         <h2>{title}</h2>
-        {link && (
-          <a href={link} className="movie-see-all">
-            See All
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7.5 15L12.5 10L7.5 5"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </a>
-        )}
+        
       </div>
       <div className="movie-grid">
         {children}
