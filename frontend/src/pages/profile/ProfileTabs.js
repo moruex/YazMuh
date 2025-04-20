0ea0102b8092d0806e7dbea829266dd731abd834
crusinistaken <l2211012020@ogr.sdu.edu.tr>
2025-04-20 18:57:54 +0300

    Semih Çantal

--- frontend/src/pages/profile/ProfileTabs.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const ProfileTabs = ({ activeTab, setActiveTab }) => {
-    return (_jsxs("div", { className: "profile-tabs", children: [_jsx("button", { className: activeTab === 'profile' ? 'active' : '', onClick: () => setActiveTab('profile'), children: "Edit Profile" }), _jsx("button", { className: activeTab === 'lists' ? 'active' : '', onClick: () => setActiveTab('lists'), children: "My Lists" }), _jsx("button", { className: activeTab === 'settings' ? 'active' : '', onClick: () => setActiveTab('settings'), children: "Account Settings" })] }));
-};
-export default ProfileTabs;
