2839a8b69ac508192010d03122de0da43efe4e18
Abdullah <133877676+akin1176@users.noreply.github.com>
2025-04-20 23:30:48 +0300

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -12,7 +12,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  // ssl: {
+  //ssl: {
   //   rejectUnauthorized: true,
   //   ca: fs.readFileSync("./ca.pem").toString(),
   // },
