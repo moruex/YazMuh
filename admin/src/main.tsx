a438d80b597e759f34a6ec6d829fa20b48185e39
ylyas2004 <yylkybayewylyas@gmail.com>
2025-06-12 18:24:30 +0300

    Ylyas Yylkybayev

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,15 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './pages/app/App.js'
 
 import './index.css'
 import client from '@graphql/apolloClient.js'
 import { ApolloProvider } from '@apollo/client'
+import { AuthProvider } from '@contexts/AuthContext'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </ApolloProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
